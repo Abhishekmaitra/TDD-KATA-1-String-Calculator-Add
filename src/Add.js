4adee0a64330581bb2266b8066fa6a0667ba8ac7
Abhishek Maitra <abhishekmaitra18@gmail.com>
2022-06-12 12:42:28 +0530

    Fixing Test Case 02,03,04

--- src/Add.js
+++ src/Add.js
@@ -2,5 +2,11 @@ function Add(str) {
   if (str === "") {
     return 0;
   }
+  let finalArray = str.split(",");
+  let sum = 0;
+  for (let i = 0; i < finalArray.length; i++) {
+    sum += parseInt(finalArray[i]);
+  }
+  return sum;
 }
 module.exports = Add;
